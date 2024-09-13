a7eb5f600908e54bcd295389157f9cffb05d25be
marcomaddo <profmaddo@gmail.com>
2024-09-12 20:51:12 -0400

    Criação das Classes PedidoDTO e Pedido

---
+++ src/main/kotlin/tssti/fullstack/backend_kotlin_rest_api/entity/Pedido.kt
@@ -0,0 +1,19 @@
+package tssti.fullstack.backend_kotlin_rest_api.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.math.BigDecimal
+import java.util.UUID
+
+@Entity
+data class Pedido(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    val codigoPedido: UUID = UUID.randomUUID(),
+    val quantidade: Int = 0,
+    val preco: BigDecimal = BigDecimal.ZERO,
+    // val cliente: Cliente? = null,
+    // val produto: Produto? = null,
+)
