ca40ffa19dcd8150455065103c9f831af69d8f5a
marcomaddo <profmaddo@gmail.com>
2024-09-15 18:28:31 -0400

    Scripts com as Migrations do Banco de Dados

---
+++ src/main/resources/db/migration/V7__insert_categoria.sql
@@ -0,0 +1,14 @@
+insert into categoria (nome, unidade) values
+('Categoria 1',0),
+('Categoria 2',1),
+('Categoria 3',2),
+('Categoria 4',3),
+('Categoria 5',0),
+('Categoria 6',1),
+('Categoria 7',2),
+('Categoria 8',3),
+('Categoria 9',3),
+('Categoria 10',3),
+('Categoria 11',3),
+('Categoria 12',3),
+('Categoria 13',3)
