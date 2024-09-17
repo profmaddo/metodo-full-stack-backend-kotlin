9223ae752c58bdd00c786bf7b03de9086b0f8140
marcomaddo <profmaddo@gmail.com>
2024-09-17 18:52:57 -0400

    Refactor ClienteController HttpStatus Code

--- src/main/kotlin/tssti/fullstack/backend_kotlin_rest_api/controller/ClienteController.kt
+++ src/main/kotlin/tssti/fullstack/backend_kotlin_rest_api/controller/ClienteController.kt
@@ -1,6 +1,5 @@
 package tssti.fullstack.backend_kotlin_rest_api.controller
 
-import jakarta.persistence.EntityNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
