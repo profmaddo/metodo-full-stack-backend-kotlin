041969cb311d846f82ddc9e3b650c50bf7630a86
marcomaddo <profmaddo@gmail.com>
2024-09-17 10:20:37 -0400

    SwaggerConfig documentação da API do Backend

--- src/main/kotlin/tssti/fullstack/backend_kotlin_rest_api/doc/SwaggerConfig.kt
+++ src/main/kotlin/tssti/fullstack/backend_kotlin_rest_api/doc/SwaggerConfig.kt
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 
 /**
- * swagger doc API
+ * Swagger Documentação da API
  */
 class SwaggerConfig {
     @Bean
